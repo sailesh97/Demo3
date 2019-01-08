ee6f799dc3b3fb3319889936809924e7bdb27e61
sailesh97 <33670045+sailesh97@users.noreply.github.com>
2019-01-09 01:05:59 +0530

    Updates

---
+++ precache-manifest.cba4adc365cd6dca683256b876acd0c0.js
@@ -0,0 +1,22 @@
+self.__precacheManifest = [
+  {
+    "revision": "52a39d23afcf055e6b34",
+    "url": "/Demo3/static/js/runtime~main.52a39d23.js"
+  },
+  {
+    "revision": "778d1b20fc652e5065bf",
+    "url": "/Demo3/static/js/main.778d1b20.chunk.js"
+  },
+  {
+    "revision": "85bda7a2537bf425896f",
+    "url": "/Demo3/static/js/1.85bda7a2.chunk.js"
+  },
+  {
+    "revision": "778d1b20fc652e5065bf",
+    "url": "/Demo3/static/css/main.73fb620b.chunk.css"
+  },
+  {
+    "revision": "f418aea4493ae0110d6e1d29a6b6ab58",
+    "url": "/Demo3/index.html"
+  }
+];
